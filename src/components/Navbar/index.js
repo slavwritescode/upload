ee35b05d577bc61f50201c7c9b889673e14ed366
slavwritescode <preslav.getov@telusinternational.com>
2024-11-27 10:41:06 +0200

    scroll bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { renderToString } from 'react-dom/server';
 import { auth } from '../../firebase/config'
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { realtimeDb } from '../../firebase/config';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateUserInfo } from '../../Redux/Features/userInfo';
 
 import './index.css';
 import telusLogo from './telusLogo.png';
-import Constants from '../Constants';
-import appInfo from '../../../package.json';
 
 const Navbar = ({ setUserId, showStats, setShowStats, showLog, setShowLog, showDeliveryTargetsTable, setShowDeliveryTargetsTable }) => {
     const userInfo = useSelector((state) => state.userInfo.value) || {};
@@ -39,6 +34,7 @@ const Navbar = ({ setUserId, showStats, setShowStats, showLog, setShowLog, showD
         <span id="projectName">| Tokoro</span>
         {/* {userRole === 'admin' ? <a href="/registration-form" onClick={(e) => { e.preventDefault(); navigate("/registration-form"); }}>Register new moderator</a> : ''} */}
         {name && userRole === 'moderator' ? <p id="userName">Welcome, {name}</p> : ''}
+        {userRole !== 'moderator' ? <a href="/login" onClick={(e) => { e.preventDefault(); navigate("/login"); }}>Login</a> : ''}
         {<a href="/registration-form" onClick={(e) => { e.preventDefault(); navigate("/registration-form"); }}>Register new moderator</a>}
         {userRole === 'moderator' ? <a href="/video-tagging" onClick={(e) => { e.preventDefault(); navigate("/video-tagging"); }}>Video Tagging</a> : ''}
 
